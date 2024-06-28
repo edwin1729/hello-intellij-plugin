a9c34aab2f5d42c397b9b926bd7d33825f98c5b6
GitHub Action <action@github.com>
2024-06-28 19:10:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/edwin1729/hellointellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/edwin1729/hellointellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.edwin1729.hellointellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/edwin1729/hellointellijplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/edwin1729/hellointellijplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.edwin1729.hellointellijplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
